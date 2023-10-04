9673a7cbd447974e71baa1d9a0734cec5ab0698c
TabodaT <octa_draghici@yahoo.com>
2023-10-04 17:37:21 +0300

    testing

--- src/main/java/com/example/scrapping/MotoSpecsScrapper.java
+++ src/main/java/com/example/scrapping/MotoSpecsScrapper.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MotoSpecsScrapper {
 	public static void main(String[] args) throws Exception {
 		SpringApplication.run(MotoSpecsScrapper.class, args);
+		// testing commits 
 	}
 }
 
