0ffc552ef630c58ba8c51203cf8539ca37891d5c
TabodaT <octa_draghici@yahoo.com>
2024-01-04 17:03:50 +0200

    Managed to extract Spec fields 2

--- src/main/java/com/example/scrapping/service/ModelDetailsService.java
+++ src/main/java/com/example/scrapping/service/ModelDetailsService.java
@@ -11,9 +11,7 @@ import org.springframework.stereotype.Service;
 
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
-import java.util.TreeSet;
 
 @Service
 @Slf4j
